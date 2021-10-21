56b672970ac8a483f11d83afab62c45ce03110c3
DESKTOP-4EGO68O\Natawat <natawat14277@hotmail.com>
2021-10-21 23:04:01 +0700

    edit responsive and router

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38ee20384ce34f23c535e9b424dc8a3d.js"
+  "/precache-manifest.afb165f9e1d2e1496e2ffb5a23eeb7ec.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "covid-dashboard"});
